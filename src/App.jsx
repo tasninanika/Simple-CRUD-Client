292720b1cf3b6058724d13dbf8f74135cadaebbb
anikatasnin <jarintasnin09@gmail.com>
2025-03-26 22:53:59 +0600

    create a form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
