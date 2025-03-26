f401292e23a3ce11860bf07044859ffe9c56c1c7
anikatasnin <jarintasnin09@gmail.com>
2025-03-26 22:56:36 +0600

    get data from the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 
 function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.name.value;
+
+    console.log(name, email);
+  };
+
   return (
     <>
       <h1>Simple CRUD</h1>
-      <form>
+      <form onSubmit={handleAddUser}>
         <input type="text" name="name" />
         <br />
         <input type="email" name="email" />
