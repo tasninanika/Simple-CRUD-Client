0a8a6caae2773781d3efeb4a9c57a3d7b0e16d9b
anikatasnin <jarintasnin09@gmail.com>
2025-03-27 10:58:52 +0600

    post fetched users data

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,13 @@ function App() {
 
     console.log(user);
 
-    fetch("http://localhost:5000/users")
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
       .then((res) => res.json())
       .then((data) => console.log(data));
   };
